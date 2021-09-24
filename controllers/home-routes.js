85694876c73c3e9a6730157ad261983c07275da9
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-24 14:21:03 -0400

    added get route for signup on homeroutes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -79,7 +79,8 @@ router.get('/post/:id', (req, res) => {
 
             res.render('single-post', {
                 post,
-                loggedIn: req.session.loggedIn
+                loggedIn: req.session.loggedIn,
+
             });
         })
         .catch(err => {
@@ -95,6 +96,15 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
 });
 
 module.exports = router;
